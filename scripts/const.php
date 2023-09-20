bbfe357b9e30fabf6161e63c48a1d70a770ca63d
MathieuH <56202917+MatHuguet@users.noreply.github.com>
2023-09-20 22:00:24 +0200

    pg test

--- scripts/const.php
+++ scripts/const.php
@@ -7,7 +7,7 @@ const HOST = 'localhost';
 const DB_USER = 'matHu';
 const DB_PASS = 'Nuggyroots89';
 const DSN = 'mysql:host=' . HOST . ';dbname=' . DB_NAME;
-*/
+
 const DB_NAME = 'heroku_7c9d6d65ab65d8b';
 const DB_USER = 'b17bb8fb1a6a05';
 const DB_PASS = 'd051faf9';
@@ -15,14 +15,23 @@ const HOST = 'eu-cdbr-west-03.cleardb.net';
 const DSN = 'mysql://b17bb8fb1a6a05:d051faf9@eu-cdbr-west-03.cleardb.net/heroku_7c9d6d65ab65d8b?reconnect=true';
 //const DSN = 'mysql://' . DB_USER . ':' . DB_PASS . '@' . HOST . '/' . DB_NAME;
 
-/*
+
 Utilisateur: wiky8736_matHu
 Base de données: wiky8736_garage_parrot_db
 hôte: 109.234.162.234
-*/
-/*
+
+
 const DB_NAME = 'wiky8736_garage_parrot_db';
 const DB_USER = 'wiky8736_matHu';
 const DB_PASS = 'Nuggyroots89';
 const HOST = '109.234.162.234';
 const DSN = 'mysql:host=' . HOST . ';dbname=' . DB_NAME, DB_USER, DB_PASS;
+*/
+
+const HOST = 'ec2-18-202-8-133.eu-west-1.compute.amazonaws.com';
+const DB_USER = 'ageqvwrfpcnoub';
+const DB_PASS = '35789b6cc98a12aec3d5554cb7d5726e8b616c86988d7c3454ce26ccb8fa82b3';
+const DB_NAME = 'dgl19mjfthh';
+const DB_PORT = '5432';
+const DSN = 'pgsql:host=' . HOST . ';port=' . DB_PORT . ';dbname=' . DB_NAME . ';user=' . DB_USER . ';password=' . DB_PASS;
+//pgsql:host=localhost;port=5432;dbname=testdb;user=bruce;password=mypass
