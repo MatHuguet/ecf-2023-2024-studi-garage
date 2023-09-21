3e45468f6ffc8081bf1a3c53d0770ac749303326
MathieuH <56202917+MatHuguet@users.noreply.github.com>
2023-09-21 11:05:35 +0200

    add correction to review form script

--- scripts/review-form-script.php
+++ scripts/review-form-script.php
@@ -1,11 +1,27 @@
 <?php
-global $dbco;
-require_once './dbinit.php';
+
+require_once './const.php';
 
 
 
     try {
+
+        $dbco = new PDO(DSN);
+        $dbco->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        $dbco->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_BOTH);
+
         if (isset($_POST['submit'])) {
+
+            $userName = $_POST['nom'];
+            $userFirstName = $_POST['prenom'];
+            $userMail = $_POST['email'];
+            $userNote = $_POST['note'];
+            $reviewDate = date('j/m/Y');
+            $userVisit = $_POST['date'];
+            $reviewText = $_POST['message'];
+            $isValid = false;
+            $isRead = false;
+
             $stmt = $dbco->prepare("INSERT INTO user_reviews(
                          userName, 
                          userFirstName, 
@@ -37,15 +53,7 @@ require_once './dbinit.php';
             $stmt->bindParam('isvalid', $isValid);
             $stmt->bindParam('isread', $isRead);
 
-            $userName = $_POST['nom'];
-            $userFirstName = $_POST['prenom'];
-            $userMail = $_POST['email'];
-            $userNote = $_POST['note'];
-            $reviewDate = date('j/m/Y');
-            $userVisit = $_POST['date'];
-            $reviewText = $_POST['message'];
-            $isValid = false;
-            $isRead = false;
+
 
             $stmt->execute();
             echo 'Formulaire validé';
